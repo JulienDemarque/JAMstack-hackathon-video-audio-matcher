943bb9428b4747a1d16e219bcabc459fe6190d77
julien demarque <julien_demarque@hotmail.fr>
2018-11-02 18:15:00 +0200

    run lint on src, fixed travis script

--- src/connectors/apollo.js
+++ src/connectors/apollo.js
@@ -1,14 +1,14 @@
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import fetch from 'node-fetch'
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import fetch from 'node-fetch';
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    fetch,
-    uri: 'http://fake.graphql.guru/graphql',
-  }),
-  cache: new InMemoryCache(),
-})
+	link: new HttpLink({
+		fetch,
+		uri: 'http://fake.graphql.guru/graphql',
+	}),
+	cache: new InMemoryCache(),
+});
 
-export default client
+export default client;
