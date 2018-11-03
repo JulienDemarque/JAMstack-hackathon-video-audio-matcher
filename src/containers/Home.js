090ceb3c3184dfdbe7cf0b72ab383c4df8871390
Adrian Wix <adrianwix18@gmail.com>
2018-11-03 23:48:01 +0100

    Merge solve Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import { withSiteData } from 'react-static';
 import { Link } from 'react-static';
-<<<<<<< HEAD
-import { Layout } from '../styles/layout';
-//
-import logoImg from '../logo.png';
 
-export default withSiteData(() => (
-	<Layout>
-		<h2>Welcome to Audio Video Matcher</h2>
-		<p>
-			This website allows you to quickly found suitable music for your video. It
-			is using image recognition on your video to determine a best fitting
-			musical genre to it. You can then play the song of your choice along your
-			video. Ready to get started? Follow the link: <Link to="/test">Test</Link>
-		</p>
-	</Layout>
-));
-=======
 // @material-ui/core
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
@@ -66,4 +50,3 @@ class Home extends React.Component {
 
 const StyledHome = withStyles(styles)(Home);
 export default withSiteData(StyledHome);
->>>>>>> Material Header and Home
